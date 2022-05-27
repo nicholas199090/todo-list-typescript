cb0ec0fc96c7ce510a5e8bb0b6598752ee0f3bf7
nicholascaminati <nicholas.caminati.nc@gmail.com>
2022-05-27 10:30:12 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,75 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from "uuid"
 
-import confetti from 'canvas-confetti';
+let list = document.querySelector <HTMLUListElement> ('#list')
+let taskForm = document.querySelector <HTMLFormElement> ('#new-task-form')
+let tasktitle = document.querySelector <HTMLInputElement> ('#new-task-title')
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  created_at: Date
+}
+
+let tasks: Task[] =  loadTasks()
+
+tasks.forEach(addListItem)
+
+taskForm?.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (tasktitle?.value == null || tasktitle.value == "") return
+  let task = {
+    id: uuidV4(),
+    title: tasktitle.value,
+    completed: false,
+    created_at: new Date()
+  }
+  tasks.push(task)
+  addListItem(task)
+  tasktitle.value = "";
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  item.classList.add("item");
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  const button = document.createElement('button')
+  //button.setAttribute('button-id',task.id)
+  button.classList.add("remove")
+  button.textContent = "X";
+  checkbox.addEventListener("change", () =>{
+    task.completed = checkbox.checked
+    saveTasks();
+  })
+ 
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed
+  label.append(checkbox,task.title);
+  item.append(label);
+  item.append(button)
+  list?.append(item)
+  button.addEventListener('click',() =>{
+    let taskID = task.id;
+    removeListItem(taskID)
+    saveTasks()
+    item.remove()
+  })
+  saveTasks();
+}
+
+function removeListItem(itemId: string){
+  tasks = tasks.filter(data => data.id != itemId);
+  
+}
+
+function saveTasks(){
+  localStorage.setItem("TASK",JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[]{
+  let taskJSON = localStorage.getItem('TASK');
+  if(taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
